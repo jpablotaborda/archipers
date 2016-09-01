21df50f2f48c499283a003ac8f816c3303b29381
jpablotaborda <jpablo.taborda@udea.edu.co>
2016-09-01 09:48:59 -0500

    cambios

--- Ejercicioslayout/app/src/main/java/com/example/jpablotaborda/ejercicioslayout/MainActivity.java
+++ Ejercicioslayout/app/src/main/java/com/example/jpablotaborda/ejercicioslayout/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.jpablotaborda.ejercicioslayout;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void otradistri(View v){
+        Intent lanzar_ven= new Intent(this, distrib.class);
+        startActivity(lanzar_ven);
+        this.finish();
     }
 }
---
+++ Ejercicioslayout/app/src/main/java/com/example/jpablotaborda/ejercicioslayout/sorpresa.java
@@ -0,0 +1,13 @@
+package com.example.jpablotaborda.ejercicioslayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class sorpresa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sorpresa);
+    }
+}
