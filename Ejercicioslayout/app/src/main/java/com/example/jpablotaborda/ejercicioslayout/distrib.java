601497920d44286b1dfc3f0535c72ad56003a5eb
jpablotaborda <jpablo.taborda@udea.edu.co>
2016-09-01 12:40:30 -0500

    cambios 3

--- Ejercicioslayout/app/src/main/java/com/example/jpablotaborda/ejercicioslayout/distrib.java
+++ Ejercicioslayout/app/src/main/java/com/example/jpablotaborda/ejercicioslayout/distrib.java
@@ -1,7 +1,9 @@
 package com.example.jpablotaborda.ejercicioslayout;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class distrib extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class distrib extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_distrib);
+    }
+    public void asorpresa(View v){
+        Intent lan_sorpresa= new Intent(this, sorpresa.class);
+        startActivity(lan_sorpresa);
+        this.finish();
+
     }
 }
